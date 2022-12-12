532b1d941d0694b4c0e6625fc2ba0c7d97a7789f
MarijaTrpovska <marija.trpovska@gmail.com>
2022-12-12 20:42:15 +0100

    mongoose connection and schema

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("MONGO CONNECTION ERROR!!!!")
+        console.log(err)
+    })
+
+const app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'))
 
 app.get('/', (req,res) => {
     res.render('home')
+})
+
+app.get('/makecampground', async(req,res)=>{
+    const camp = new Campground({ title: 'My Backyard', description: 'cheap camping!'});
+    await camp.save();
+    res.send(camp)
 })
 
 app.listen(3000 , () => {

2a0e6885ae103bcde1c3e651a6c71637af205303
MarijaTrpovska <marija.trpovska@gmail.com>
2023-02-03 14:15:49 +0100

    basic styles added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');  //to be able to use put, patch or delete methods requests from the forms ejs templates (forms only send get or post request from the browser)
 const Campground = require('./models/campground');
 
@@ -15,6 +16,7 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 
--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,13 @@ const seedDB = async() => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor((Math.random()*1000));
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, fuga accusamus iure corporis in nostrum assumenda asperiores doloribus consequatur. Accusantium eligendi sunt ipsum doloribus aliquid veritatis praesentium eos non dolor.',
+            price
         })
         await camp.save();
     }
